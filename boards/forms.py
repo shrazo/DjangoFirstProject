e2d34981e4fb478b8bc457f2d3bd8ba62fd73718
shraju024 <saddam.hossain.raju.024@gmail.com>
2019-06-26 17:38:09 +0000

    add a contact html file

--- boards/forms.py
+++ boards/forms.py
@@ -20,14 +20,11 @@ class NewTopicForm(forms.ModelForm):
 class ContactForm(forms.ModelForm):
     full_name = forms.CharField(
         max_length=200,
-    help_text='The max length of the text id 4000.',
-    widget=forms.Textarea(attrs={'placeholder':'Full name'})
-    )
-    email = forms.EmailField(
-    widget=forms.Textarea(attrs={'placeholder':'E-mail Address'})
-    )
+        help_text='The max length of the text id 4000.',
+        widget=forms.Textarea(attrs={'placeholder': 'Full name'}))
+    email = forms.EmailField(widget=forms.Textarea(
+        attrs={'placeholder': 'E-mail Address'}))
     phone = forms.CharField(
-    widget=forms.Textarea(attrs={'placeholder':'Phone Number'}),
-    help_text='e.g. +8801737520527'
-    )
-    message = forms.forms.TextInput(attrs={'size': 10, 'title': 'Your name'})
+        widget=forms.Textarea(attrs={'placeholder': 'Phone Number'}),
+        help_text='e.g. +8801737520527')
+    message = forms.TextInput(attrs={'size': 10, 'title': 'Your name'})
